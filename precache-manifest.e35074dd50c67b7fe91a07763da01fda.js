b6f9b0a8aca183692db0ab907a0189c1256fcff7
Michael Cota <mikecotahf@gmail.com>
2022-07-03 08:05:08 -0400

    Updates

---
+++ precache-manifest.e35074dd50c67b7fe91a07763da01fda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47eca73e78d68131e8755483f81be44",
+    "url": "/Book-Search-Engine/index.html"
+  },
+  {
+    "revision": "71be82b12759e3732eef",
+    "url": "/Book-Search-Engine/static/css/2.1ebb0010.chunk.css"
+  },
+  {
+    "revision": "15db918a4bf4a064444d",
+    "url": "/Book-Search-Engine/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "71be82b12759e3732eef",
+    "url": "/Book-Search-Engine/static/js/2.4fe47aa3.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/Book-Search-Engine/static/js/2.4fe47aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15db918a4bf4a064444d",
+    "url": "/Book-Search-Engine/static/js/main.61573b23.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Book-Search-Engine/static/js/main.61573b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97605c75cecf991a7a7",
+    "url": "/Book-Search-Engine/static/js/runtime-main.aa81579e.js"
+  }
+]);
